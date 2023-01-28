af34162e575b89fce6d6dbc283b343d36624e133
Nicomiano19 <nicomiano19@gmail.com>
2023-01-28 12:26:15 -0300

    Comiteo 2

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -3,15 +3,16 @@
 @section('content')
 <div class="container">
 
+
     @if (Session::has('mensaje'))
 
     <div class="alert alert-success alert-dismissible" role="alert">
     
        {{ Session::get('mensaje') }}
     
-       <button type="button" class="close" data-dismiss="alert" aria-label="close">
+       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
-      </button>        
+      </button>       
     
     </div>
     
